ea2e6c6b383b2f0dcb7cc11a469c37681ff39397
Jesteban97 <jearistizabal@i4digital.com>
2024-04-08 16:59:20 -0500

    ajustes

--- src/components/utils/api-instance.ts
+++ src/components/utils/api-instance.ts
@@ -1,32 +1,31 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 export default function instanceApi(
-  baseURL: string,
-  token?: string
+  baseURL: string
 ): AxiosInstance {
   const api = axios.create({
     baseURL: baseURL,
   });
 
-  const tokenToSend = token ?? localStorage.getItem("token")
+  // const tokenToSend = token ?? localStorage.getItem("token")
 
-  api.interceptors.request.use(
-    (req:any) => {
-      if (tokenToSend) {
-        req.headers.Authorization = `Bearer ${tokenToSend}`;
-      }
-      return req;
-    },
-    (err:any) => {
-      throw err;
-    }
-  );
+  // api.interceptors.request.use(
+  //   (req: AxiosRequestConfig) => {
+  //     if (tokenToSend) {
+  //       req.headers.Authorization = `Bearer ${tokenToSend}`;
+  //     }
+  //     return req;
+  //   },
+  //   (err: AxiosError) => {
+  //     throw err;
+  //   }
+  // );
 
   api.interceptors.response.use(
-    (response:any) => {
+    (response: AxiosResponse) => {
       return response.data;
     },
-    (error:any) => {
+    (error: AxiosError) => {
       throw error;
     }
   );
